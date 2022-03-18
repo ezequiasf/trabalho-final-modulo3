7647cdd4ab3e51cbb8b2cf6effb216fc8bb86224
ezequiasf <ezequiasbarros1997@gmail.com>
2022-03-18 02:56:47 -0300

    Adição do DTO Comentario

---
+++ src/main/java/com/dbccompany/receitasapp/dto/ComentarioFormado.java
@@ -0,0 +1,15 @@
+package com.dbccompany.receitasapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ComentarioFormado extends ComentarioDTO{
+    private Integer idComentario;
+    private Integer idUsuario;
+}
--- src/main/java/com/dbccompany/receitasapp/entity/Comentario.java
+++ src/main/java/com/dbccompany/receitasapp/entity/Comentario.java
@@ -2,10 +2,6 @@ package com.dbccompany.receitasapp.entity;
 
 import lombok.*;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,15 +10,8 @@ import javax.validation.constraints.Size;
 @ToString(includeFieldNames = false, onlyExplicitlyIncluded = true)
 public class Comentario {
     private Integer idComentario;
-
-    @NotNull(message = "O id usuário deve ser informado.")
     private Integer idUsuario;
-
-    @NotNull(message = "O id da receita deve ser informado.")
     private Integer idReceita;
-
     @ToString.Include
-    @NotBlank(message = "O comentário deve ser informado.")
-    @Size(min = 1, max = 200, message = "O comentário deve ter no mínimo 1 caracter e no máximo 200.")
     private String comentario;
 }
