9016b98da53f4d3ce6800c1c5b59e9312188ca95
ezequiasf <ezequiasbarros1997@gmail.com>
2022-03-18 23:38:44 -0300

    Adição da documentação do ReceitaController e UsuarioController

--- src/main/java/com/dbccompany/receitasapp/controller/ReceitaController.java
+++ src/main/java/com/dbccompany/receitasapp/controller/ReceitaController.java
@@ -4,6 +4,9 @@ import com.dbccompany.receitasapp.dto.ReceitaDTO;
 import com.dbccompany.receitasapp.dto.ReceitaFormada;
 import com.dbccompany.receitasapp.exceptions.ObjetoNaoEncontradoException;
 import com.dbccompany.receitasapp.service.ReceitaService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -18,27 +21,47 @@ public class ReceitaController {
     @Autowired
     private ReceitaService receitaService;
 
+    @ApiOperation(value = "Retorna uma lista de receitas registradas.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "As receitas foram listadas com sucesso."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção no sistema."),})
     @GetMapping("/listarReceitas")
     public List<ReceitaFormada> listarTodasReceitas() {
         return receitaService.listarTodasReceitas();
     }
 
+    @ApiOperation(value = "Encontra uma receita registrada através do id informado.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "A receita foi informada com sucesso."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),})
     @GetMapping("/{idReceita}")
     public ReceitaFormada listarReceitaPorId(@PathVariable("idReceita") Integer idReceita) throws ObjetoNaoEncontradoException {
         return receitaService.encontrarPorId(idReceita);
     }
 
+    @ApiOperation(value = "Encontra receitas a partir de um id de usuário informado.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "As receitas foram informadas com sucesso."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),})
     @GetMapping("/usuario/{idUsuario}")
     public List<ReceitaFormada> listarReceitasPorUsuario(@PathVariable("idUsuario") Integer idUsuario) throws ObjetoNaoEncontradoException {
         return receitaService.listarReceitarPorUsuario(idUsuario);
     }
 
+    @ApiOperation(value = "Cadastra uma receita no banco de dados.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Cadastrou a receita com sucesso no banco."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),})
     @PostMapping("/salvar/{idUsuario}")
     @Validated
     public ReceitaFormada salvarReceita(@Valid @RequestBody ReceitaDTO receitaDTO, @PathVariable("idUsuario") Integer idUsuario) {
         return receitaService.salvarReceita(receitaDTO, idUsuario);
     }
 
+    @ApiOperation(value = "Atualiza uma receita no banco de dados através do id informado.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Atualizou com sucesso a receita no banco."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),})
     @PutMapping("/atualizar/{idReceita}")
     @Validated
     public ReceitaFormada atualizarReceita(@PathVariable("idReceita") Integer idReceita,
@@ -46,6 +69,10 @@ public class ReceitaController {
         return receitaService.atualizarReceita(receitaAtualizar, idReceita);
     }
 
+    @ApiOperation(value = "Deleta uma receita do banco de dados.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Deletou a receita com sucesso do banco."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),})
     @DeleteMapping("/deletar/{idReceita}")
     public ReceitaFormada deletarReceita(@PathVariable("idReceita") Integer idReceita) throws ObjetoNaoEncontradoException {
         return receitaService.deletarReceita(idReceita);
--- src/main/java/com/dbccompany/receitasapp/controller/UsuarioController.java
+++ src/main/java/com/dbccompany/receitasapp/controller/UsuarioController.java
@@ -5,6 +5,9 @@ import com.dbccompany.receitasapp.dto.UsuarioDTO;
 import com.dbccompany.receitasapp.dto.UsuarioFormado;
 import com.dbccompany.receitasapp.exceptions.ObjetoNaoEncontradoException;
 import com.dbccompany.receitasapp.service.UsuarioService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -19,22 +22,38 @@ public class UsuarioController {
     @Autowired
     private UsuarioService serviceUsuario;
 
+    @ApiOperation(value = "Retorna uma lista de usuários registrados.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Os usuários foram listadas com sucesso."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção no sistema."),})
     @GetMapping("/listarUsuarios")
     public List<UsuarioFormado> listarTodosUsuarios() {
         return serviceUsuario.listarTodosUsuarios();
     }
 
+    @ApiOperation(value = "Encontra um usuário registrado através do id informado.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "O usuário foi informado com sucesso."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),})
     @GetMapping("/{idUsuario}")
     public UsuarioFormado encontrarUsuarioPorId(@PathVariable("idUsuario") Integer idUsuario) throws ObjetoNaoEncontradoException {
         return serviceUsuario.encontrarUsuarioPorId(idUsuario);
     }
 
+    @ApiOperation(value = "Cadastra um usuário no banco de dados.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Cadastrou o usuário com sucesso no banco."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),})
     @PostMapping("/salvar")
     @Validated
     public UsuarioFormado salvarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) {
         return serviceUsuario.salvarUsuario(usuarioDTO);
     }
 
+    @ApiOperation(value = "Atualiza um usuário no banco de dados através do id informado.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Atualizou com sucesso o usuário no banco."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),})
     @PutMapping("/atualizar/{idUsuario}")
     @Validated
     public UsuarioFormado atualizarUsuario(@PathVariable("idUsuario") Integer idUsuario,
@@ -42,6 +61,10 @@ public class UsuarioController {
         return serviceUsuario.atualizarUsuario(usuarioAtualizar, idUsuario);
     }
 
+    @ApiOperation(value = "Deleta um usuário do banco de dados.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Deletou o usuáro com sucesso do banco."),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),})
     @DeleteMapping("/deletar/{idUsuario}")
     public UsuarioFormado deletarUsuario(@PathVariable("idUsuario") Integer idUsuario) throws ObjetoNaoEncontradoException {
         return serviceUsuario.deletarUsuario(idUsuario);
